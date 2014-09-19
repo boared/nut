bc58e5a3d73cfe062f90e4d8901ae95eaa033240
eaperz <eap.blitzkrieg@gmail.com>
2014-09-19 15:01:21 -0300

    Added a class to handle type conversion between OpenGL and nut engine.

---
+++ src/engine/core/opengl/GLTypeConversion.h
@@ -0,0 +1,93 @@
+/** 
+ * \file GLTypeConversion.h
+ * \brief This class is used for type conversion between OpenGL and nut engine.
+ * 
+ * @author: Eder A. Perez.
+ */
+
+#ifndef GLTYPECONVERSION_H
+#define GLTYPECONVERSION_H
+
+#include "GL/glew.h"
+#include "DataType.h"
+
+
+
+namespace nut
+{
+    class GLTypeConversion
+    {
+        public:
+
+        /**
+         * Convert an enum class Type to a GLenum type.
+         * 
+         * @param type An enum class Type.
+         * @return A corresponding GLenum. The default is GL_FLOAT.
+         */
+        static GLenum typeToGLenum(Type type)
+        {
+            GLenum glType;
+
+            switch (Type)
+            {
+                case Type::CHAR:
+                    glType = GL_BYTE;
+                    break;
+
+                case Type::INT:
+                    glType = GL_INT;
+                    break;
+
+                case Type::I8:
+                    glType = GL_BYTE;
+                    break;
+
+                case Type::I16:
+                    glType = GL_SHORT;
+                    break;
+
+                case Type::I32:
+                    glType = GL_INT;
+                    break;
+
+                case Type::UCHAR:
+                    glType = GL_UNSIGNED_BYTE;
+                    break;
+                    break;
+
+                case Type::UINT:
+                    glType = GL_UNSIGNED_INT;
+                    break;
+
+                case Type::U8:
+                    glType = GL_UNSIGNED_BYTE;
+                    break;
+
+                case Type::U16:
+                    glType = GL_UNSIGNED_SHORT;
+                    break;
+
+                case Type::U32:
+                    glType = GL_UNSIGNED_INT;
+                    break;
+
+                case Type::FLOAT:
+                    glType = GL_FLOAT;
+                    break;
+
+                case Type::DOUBLE:
+                    glType = GL_DOUBLE;
+                    break;
+
+                default:
+                    glType = GL_FLOAT;
+                    break;
+            }
+
+            return glType;
+        }
+    };
+}
+
+#endif // GLTYPECONVERSION_H
