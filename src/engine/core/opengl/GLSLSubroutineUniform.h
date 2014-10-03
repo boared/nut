6be46b60245b98875b56f9b37b4f897549917111
eaperz <eap.blitzkrieg@gmail.com>
2014-10-03 16:38:35 -0300

    Adding GLSL variable classes.

---
+++ src/engine/core/opengl/GLSLSubroutineUniform.h
@@ -0,0 +1,44 @@
+/** 
+ * \file GLSLSubroutineUniform.h
+ * \brief This class holds information about a GLSL subroutine uniform.
+ * 
+ * @author: Eder A. Perez.
+ */
+ 
+#ifndef GLSLSUBROUTINEUNIFORM_H
+#define GLSLSUBROUTINEUNIFORM_H
+
+#include <string>
+#include <vector>
+#include "GL/glew.h"
+
+
+
+namespace nut
+{
+    class GLSLSubroutineUniform
+    {
+        public:
+
+        GLuint program; /**< An OpenGL program where this subroutine uniform is specified for. */
+        GLint location; /**< The location of the subroutine uniform. If this subroutine uniform is an array, the location in the i-th index is location + i. */
+        GLenum shaderType; /**< A subroutine uniform is always associated to only one shader stage. */
+        std::string name; /**< The name of the subroutine uniform. */
+        std::vector<GLint> compatibleSubroutines; /**< List with indices of all compatible subroutines. */
+        GLint uniformArraySize; /*< Number of elements in case this subroutine uniform is an array, 1 if it is not. */
+
+
+
+        GLSLSubroutineUniform() :
+            program(0),
+            location(-1),
+            shaderType(GL_INVALID_ENUM),
+            name(0),
+            compatibleSubroutines(),
+            uniformArraySize(0)
+        {
+        }
+    };
+}
+
+#endif // GLSLSUBROUTINEUNIFORM_H
--- src/engine/core/opengl/GLSLVariable.h
+++ src/engine/core/opengl/GLSLVariable.h
@@ -1,6 +1,6 @@
 /** 
- * \file GLProgram.h
- * \brief This class stores information about a GLSL attribute or uniform variable.
+ * \file GLSLVariable.h
+ * \brief This class holds information about a GLSL attribute or uniform variable.
  * 
  * @author: Eder A. Perez.
  */
@@ -15,36 +15,28 @@
 
 namespace nut
 {
-     class GLSLVariable
+    class GLSLVariable
     {
         public:
+
+        GLuint program; /**< An OpenGL program where this variable is specified for. */
+        GLint location; /**< The location of the variable. */
+        std::string name; /**< The name of the variable. */
+        GLint size; /**< The size of the variable. */
+        GLenum type; /**< The type of the variable. */
+        bool isUniform; /**< True if it is an uniform variable. False if it is an attribute. */
+
+
 
         GLSLVariable() :
             program(0),
             location(-1),
+            name(0),
             size(0),
             type(0),
-            name(0),
             isUniform(false)
         {
         }
-
-        GLSLVariable(GLuint program, GLint location, GLint size, GLenum type, const char* name, bool isUniform) :
-            program(program),
-            location(location),
-            size(size),
-            type(type),
-            name(name),
-            isUniform(isUniform)
-        {
-        }
-        
-        const GLuint program; /**< An OpenGL program where this variable is specified for. */
-        const GLint location; /**< The location of the variable. */
-        const GLint size; /**< The size of the variable. */
-        const GLenum type; /**< The type of the variable. */
-        const std::string name; /**< The name of the variable. */
-        const bool isUniform; /**< True if it is an uniform variable. False if it is an attribute. */
     };
 }
 
